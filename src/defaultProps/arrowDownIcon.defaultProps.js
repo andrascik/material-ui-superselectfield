b520da2caedc5b8f2985186273cb866a60f6deff
Richard <richard@andrascik.eu>
2019-01-19 12:58:29 +0100

    ArrowDropDown icon update

--- src/defaultProps/arrowDownIcon.defaultProps.js
+++ src/defaultProps/arrowDownIcon.defaultProps.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import DropDownArrow from 'material-ui/svg-icons/navigation/arrow-drop-down';
+import DropDownArrow from '@material-ui/icons/ArrowDropDown';
 
 export default {
   customIcon: <DropDownArrow />,
